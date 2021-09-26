bc37a81eae09aa4e524b9d14ba07aa57f7b0de5f
Coffebean <istvan.mayer1999@protonmail.com>
2021-09-27 00:22:09 +0200

    Hopefully this is the working of the device location

--- app/src/main/java/com/example/testingmaps/MapsActivity.kt
+++ app/src/main/java/com/example/testingmaps/MapsActivity.kt
@@ -4,24 +4,42 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 
 internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
-    GoogleMap.OnMyLocationButtonClickListener,GoogleMap.OnMyLocationClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
+    GoogleMap.OnMyLocationButtonClickListener,GoogleMap.OnMyLocationClickListener, ActivityCompat.OnRequestPermissionsResultCallback,
+    GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private lateinit var mMap: GoogleMap
     private var permissionDenied = false
+    internal lateinit var mLastLocation: Location
+    internal lateinit var mLocationResult: LocationRequest
+    internal lateinit var mLocationCallback: LocationCallback
+    internal var mCurrLocationMarker: Marker? = null
+    internal var mGoogleApiClient: GoogleApiClient? = null
+    internal lateinit var mLocationRequest: LocationRequest
+    internal var mFusedLocationClient: FusedLocationProviderClient? = null
 
     //Request for location access on device
     private val requestPermissionLauncher =
@@ -80,6 +98,8 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
         enableMyLocation()
 
         Toast.makeText(this, "${lat}, ${long}", Toast.LENGTH_LONG).show()
+
+
     }
 
     /**
@@ -87,14 +107,27 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
      */
     @SuppressLint("MissingPermission")
     private fun enableMyLocation() {
-        if (!::mMap.isInitialized) return
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-            == PackageManager.PERMISSION_GRANTED) {
-            mMap.isMyLocationEnabled = true
+
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                buildGoogleApiClient()
+                mMap!!.isMyLocationEnabled = true
+            }
         } else {
+            buildGoogleApiClient()
+            mMap!!.isMyLocationEnabled = true
         }
     }
-
+    @Synchronized
+    protected fun buildGoogleApiClient() {
+        mGoogleApiClient = GoogleApiClient.Builder(this)
+            .addConnectionCallbacks(this)
+            .addOnConnectionFailedListener(this)
+            .addApi(LocationServices.API).build()
+        mGoogleApiClient!!.connect()
+    }
 
 
     override fun onMyLocationButtonClick(): Boolean {
@@ -133,5 +166,51 @@ internal class MapsActivity : AppCompatActivity(), OnMapReadyCallback,
          * @see .onRequestPermissionsResult
          */
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onConnected(bundle: Bundle?) {
+
+        mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 1000
+        mLocationRequest.fastestInterval = 1000
+        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            mFusedLocationClient?.requestLocationUpdates(mLocationRequest,mLocationCallback, Looper.myLooper())
+        }
+    }
+
+    fun onLocationChanged(location: Location) {
+
+        mLastLocation = location
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker!!.remove()
+        }
+        //Place current location marker
+        val latLng = LatLng(location.latitude, location.longitude)
+        val markerOptions = MarkerOptions()
+        markerOptions.position(latLng)
+        markerOptions.title("Current Position")
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+        mCurrLocationMarker = mMap!!.addMarker(markerOptions)
+
+        //move map camera
+        mMap!!.moveCamera(CameraUpdateFactory.newLatLng(latLng))
+        mMap!!.animateCamera(CameraUpdateFactory.zoomTo(11f))
+
+        //stop location updates
+        if (mGoogleApiClient != null) {
+            mFusedLocationClient?.removeLocationUpdates(mLocationCallback)
+        }
+    }
+
+    override fun onConnectionFailed(connectionResult: ConnectionResult) {
+        Toast.makeText(applicationContext,"connection failed", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+        Toast.makeText(applicationContext,"connection suspended", Toast.LENGTH_SHORT).show()
     }
 }
